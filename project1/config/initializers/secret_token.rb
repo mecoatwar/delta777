1c94bd94485d992994faf6abee8de6beccaa211a
daniel <mecoatwar@gmail.com>
2014-05-15 22:14:14 -0700

    Devise Works PRAISE JESUS

---
+++ project1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '99808494831a95665facd25d1c6c592d2b38d4d7af32df1aea0ab301b7e4857ccbd1f93387fe83a9b70ed2a71c2fec648cb6a63a8d1d84a2e88806edef92fdf7'
